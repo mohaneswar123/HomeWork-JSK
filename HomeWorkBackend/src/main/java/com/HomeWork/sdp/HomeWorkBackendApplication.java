0b6a214e6964f808e24c28838c12e8ce8ba5ae01
Eswar <pvmohaneswar@gmail.com>
2025-08-24 23:51:21 +0530

    first commit -v5

--- HomeWorkBackend/src/main/java/com/HomeWork/sdp/HomeWorkBackendApplication.java
+++ HomeWorkBackend/src/main/java/com/HomeWork/sdp/HomeWorkBackendApplication.java
@@ -2,9 +2,10 @@ package com.HomeWork.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HomeWorkBackendApplication {
+public class HomeWorkBackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeWorkBackendApplication.class, args);
